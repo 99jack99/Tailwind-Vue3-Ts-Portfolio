410f39651d5684ec4f3004ae4794f0f284e41306
Jackson Almeida <jackson.almeida@dismuntel.local>
2023-10-17 11:11:37 +0200

    (feat): responsive main view

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -2,33 +2,32 @@
 
 <template>
   <div class="h-screen">
-    <div
-      class="grid grid-cols-3 grid-rows-3 md:grid-cols-3 md:grid-rows-3 h-full"
-    >
+    <div class="flex flex-col md:grid md:grid-cols-3 md:grid-rows-3 h-full">
       <div
-        class="col-span-2 md:col-span-2 border-4 border-black grid justify-items-center items-center cursor-pointer hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500"
+        class="h-48 md:h-auto md:col-span-2 border-4 border-black grid place-content-center cursor-pointer hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-500"
       >
-        <h1 class="text-6xl font-semibold">INTRO</h1>
+        <h1 class="text-5xl md:text-6xl font-semibold">INTRO</h1>
       </div>
       <div
-        class="row-span-2 md:row-span-2 border-4 border-black grid justify-items-center items-center cursor-pointer hover:bg-gradient-to-r hover:from-violet-500 hover:to-fuchsia-500"
+        class="h-48 md:h-auto md:row-span-2 md:col-span-1 border-4 border-black grid place-content-center cursor-pointer hover:bg-gradient-to-r hover:from-violet-500 hover:to-fuchsia-500"
       >
-        <h1 class="text-6xl font-semibold">ABOUT ME</h1>
-      </div>
-      <div
-        class="row-span-2 md:row-span-2 border-4 border-black grid justify-items-center items-center hover:bg-gradient-to-r hover:from-amber-500 hover:to-orange-500"
-      >
-        <h1 class="text-6xl font-semibold">EXPERIENCE</h1>
+        <h1 class="text-5xl md:text-6xl font-semibold">ABOUT ME</h1>
       </div>
 
       <div
-        class="border-4 border-black img bg-center bg-cover bg-no-repeat"
+        class="truncate md:text-clip h-48 md:h-auto md:row-span-2 md:col-span-1 border-4 border-black grid place-content-center hover:bg-gradient-to-r hover:from-amber-500 hover:to-orange-500"
+      >
+        <h1 class="text-5xl md:text-6xl font-semibold">EXPERIENCE</h1>
+      </div>
+
+      <div
+        class="h-48 md:h-auto border-4 border-black img bg-center bg-cover bg-no-repeat order-first md:order-none"
       ></div>
 
       <div
-        class="col-span-2 md:col-span-2 border-4 border-black grid justify-items-center items-center hover:bg-gradient-to-r hover:from-red-500 hover:to-pink-500"
+        class="h-48 md:h-auto md:row-span-1 md:col-span-2 border-4 border-black grid place-content-center hover:bg-gradient-to-r hover:from-red-500 hover:to-pink-500"
       >
-        <h1 class="text-6xl font-semibold">CONTACT</h1>
+        <h1 class="text-5xl md:text-6xl font-semibold">CONTACT</h1>
       </div>
     </div>
   </div>
