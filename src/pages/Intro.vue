279b63959b75b9e8204fd0e688b7f7117462c3a8
Jackson Almeida <jackson.almeida@dismuntel.local>
2023-10-17 16:28:24 +0200

    (feat): intro elements finished, yet to finish details and responsive

--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -16,29 +16,29 @@
 
       <div class="w-full flex justify-around">
         <div class="flex flex-col">
-          <div class="flex items-center justify-center mb-4">
+          <div class="flex items-center justify-start mb-4">
             <img src="@/assets/icons/vue.svg" class="h-10 w-10" alt="" />
             <div class="ml-2 text-lg font-semibold">Vue.js</div>
           </div>
-          <div class="flex items-center justify-center mb-4">
-            <img src="@/assets/icons/vue.svg" class="h-10 w-10" alt="" />
-            <div class="ml-2 text-lg font-semibold">Vue.js</div>
+          <div class="flex items-center justify-start mb-4">
+            <img src="@/assets/icons/ts.svg" class="h-10 w-10" alt="" />
+            <div class="ml-2 text-lg font-semibold">Typescript</div>
           </div>
-          <div class="flex items-center justify-center mb-4">
-            <img src="@/assets/icons/vue.svg" class="h-10 w-10" alt="" />
-            <div class="ml-2 text-lg font-semibold">Vue.js</div>
+          <div class="flex items-center justify-start mb-4">
+            <img src="@/assets/icons/js.svg" class="h-10 w-10" alt="" />
+            <div class="ml-2 text-lg font-semibold">Javascript</div>
           </div>
         </div>
         <div class="flex flex-col">
-          <div class="flex items-center justify-center mb-4">
+          <div class="flex items-center justify-start mb-4">
+            <img src="@/assets/icons/tailwind.svg" class="h-10 w-10" alt="" />
+            <div class="ml-2 text-lg font-semibold">Tailwind</div>
+          </div>
+          <div class="flex items-center justify-start mb-4">
             <img src="@/assets/icons/vue.svg" class="h-10 w-10" alt="" />
             <div class="ml-2 text-lg font-semibold">Vue.js</div>
           </div>
-          <div class="flex items-center justify-center mb-4">
-            <img src="@/assets/icons/vue.svg" class="h-10 w-10" alt="" />
-            <div class="ml-2 text-lg font-semibold">Vue.js</div>
-          </div>
-          <div class="flex items-center justify-center mb-4">
+          <div class="flex items-center justify-start mb-4">
             <img src="@/assets/icons/vue.svg" class="h-10 w-10" alt="" />
             <div class="ml-2 text-lg font-semibold">Vue.js</div>
           </div>
@@ -46,13 +46,33 @@
       </div>
     </div>
 
-    <div class="mt-20 h-96 w-80">
-      <div
-        class="h-4/5 bg-[url(../assets/imgs/per.jpg)] bg-cover bg-right border-4"
-      ></div>
-      <div class="h-1/5 flex flex-col justify-center p-1 mt-4">
-        <div class="text-2xl font-semibold">Lana Effors</div>
-        <div class="text-2xl font-thin">22, Software Developer</div>
+    <div class="flex justify-around items-start w-full mt-20">
+      <div class="h-96 w-80">
+        <div
+          class="h-4/5 bg-[url(../assets/imgs/per.jpg)] bg-cover bg-right border-4"
+        ></div>
+        <div class="h-1/5 flex flex-col justify-center p-1 mt-4">
+          <div class="text-2xl font-semibold">Lana Effors</div>
+          <div class="text-2xl font-thin">22, Software Developer</div>
+        </div>
+      </div>
+
+      <div class="flex flex-col w-3/4 px-10">
+        <div
+          class="bg-black border-4 border-black h-32 flex items-center justify-start cursor-crosshair"
+        >
+          <h1 class="text-6xl font-semibold ml-4 text-white">VIDEOGAMES*</h1>
+        </div>
+        <div
+          class="bg-black border-4 border-black h-32 my-10 flex items-center justify-start cursor-crosshair"
+        >
+          <h1 class="text-6xl font-semibold ml-4 text-white">SPORTS*</h1>
+        </div>
+        <div
+          class="bg-black border-4 border-black h-32 flex items-center justify-start cursor-crosshair"
+        >
+          <h1 class="text-6xl font-semibold ml-4 text-white">LEARNING*</h1>
+        </div>
       </div>
     </div>
   </div>
