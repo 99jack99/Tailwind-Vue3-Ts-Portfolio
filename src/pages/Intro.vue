7ba75d078d777ae570c87502034447f212435537
Jackson Almeida <jackson.almeida@dismuntel.local>
2023-10-20 09:54:40 +0200

    (style): fixed styled on intro top

--- src/pages/Intro.vue
+++ src/pages/Intro.vue
@@ -8,17 +8,24 @@
       LOVE, PASSION AND GLORY
     </h1>
 
-    <div class="flex flex-col sm:flex-row w-full mt-20">
+    <div
+      class="flex flex-col sm:flex-row w-full mt-20 bg-[url(../assets/imgs/form.jpg)] bg-cover bg-right py-10 border-4 border-black px-5 rounded-sm"
+    >
       <div class="text-2xl font-medium w-full sm:w-2/3">
-        I`ve been programming since I was a teen... and guess what? I only got
-        better. My passion, consistence and loved for it, made me a fierce
-        developer. Nowadays I dedicate my time in Futuristic and modern web
-        pages.
+        Programming has been an adventure... I came from playing games, and
+        internet itself. We all have some curiosity about how some things work.
+        Turns out, I enjoyed how the process of creating a web page translate in
+        understandin the concept of idea or product behind it.
+
+        <div class="mt-10 mx-10 text-normal text-gray-800">
+          “If you can do what you do best and be happy, you are further along in
+          life than most people.”
+        </div>
       </div>
 
-      <div class="w-full m-0 relative font-chalmone bg-slate-300">
+      <div class="w-full h-96 relative text-white">
         <video
-          class="border-4 border-black grayscale absolute top-0 left-0 h-min"
+          class="border-4 rounded-sm border-black grayscale absolute top-0 left-0 ml-[20%] h-96 opacity-90"
           autoplay
           muted
           loop
@@ -26,11 +33,11 @@
           <source src="@/assets/imgs/mini.mp4" type="video/mp4" />
         </video>
 
-        <h1 class="absolute top-2 left-2 ml-10 text-9xl">Less</h1>
-        <h1 class="absolute top-8 left-8 ml-10 text-6xl">is</h1>
-        <div class="absolute top-12 left-16 ml-12 text-9xl text-cyan-900">
-          <span class="line-through">MORE</span><span> better</span>
-        </div>
+        <h1 class="absolute top-2 right-2 ml-10 text-8xl">Less</h1>
+        <h1 class="absolute top-16 right-8 ml-10 text-6xl text-[#a32404]">
+          is
+        </h1>
+        <h1 class="absolute top-12 right-16 ml-12 text-8xl">Better</h1>
       </div>
     </div>
 
@@ -58,7 +65,7 @@
         <div
           class="bg-black border-4 border-black h-32 my-10 flex items-center justify-start cursor-crosshair"
         >
-          <h1 class="text-5xl sm:text-6xll font-semibold ml-4 text-white">
+          <h1 class="text-5xl sm:text-6xl font-semibold ml-4 text-white">
             SPORTS*
           </h1>
         </div>
